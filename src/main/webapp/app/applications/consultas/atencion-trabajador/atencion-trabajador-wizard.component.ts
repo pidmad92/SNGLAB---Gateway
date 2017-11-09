46a3f1be04389ecd7f2f1341769d43ce3d229add
Pedro <pedrolps12@gmail.com>
2017-11-09 01:19:13 -0500

    Lazy load, outlet
    Former-commit-id: 6a150d0a5850da21123796ae07a0d0587a6541d8

---
+++ src/main/webapp/app/applications/consultas/atencion-trabajador/atencion-trabajador-wizard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'jhi-atencion-trabajador-wizard',
+    templateUrl: 'atencion-trabajador-wizard.component.html'
+})
+export class AtencionTrabajadorWizardComponent {
+
+}
