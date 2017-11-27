627e612a5a4a4afa99573817b715c2f5ffde17d1
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-11-27 17:43:18 -0500

    Elimina lineas comentadas sin valor

--- src/main/webapp/app/applications/liquidaciones/registro-atencion/registro-atencion.route.ts
+++ src/main/webapp/app/applications/liquidaciones/registro-atencion/registro-atencion.route.ts
@@ -6,11 +6,6 @@ import { RegistroAtencionComponent } from './registro-atencion.component';
 import { UserRouteAccessService } from '../../../shared';
 import { JhiPaginationUtil } from 'ng-jhipster';
 
-// import { RevisarSolicitudesComponent } from './revisar-solicitudes.component';
-// import { AccionadopDetailComponent } from './accionadop-detail.component';
-// import { AccionadopPopupComponent } from './accionadop-dialog.component';
-// import { AccionadopDeletePopupComponent } from './accionadop-delete-dialog.component';
-
 export const RegistroAtencionRoute: Routes = [
     {
         path: 'registro-atencion',
