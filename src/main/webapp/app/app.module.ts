bd007a8625c5991d31396c4e3bcb3c9d9e591bd4
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-11-27 17:09:09 -0500

    Importa liquidacionesRoute y lo agrega como cte. LAZZY_ROUTES

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,7 +14,16 @@ import { LoginComponent } from './login/login.component';
 import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
 
-import { entityRoute, adminRoute, loginRoute, consultasRoute, defensaRoute, denunciasRoute, sindicatosRoute } from './app.routing';
+import {
+    adminRoute,
+    consultasRoute,
+    defensaRoute,
+    denunciasRoute,
+    entityRoute,
+    liquidacionesRoute,
+    loginRoute,
+    sindicatosRoute,
+} from './app.routing';
 import { GatewayprimengModule } from './primeng/primeng.module';
 
 import { GatewayTipoEntidadModule } from './entities/tipo-entidad/tipo-entidad.module';
@@ -51,6 +60,7 @@ const LAZY_ROUTES = [
     loginRoute,
     sindicatosRoute,
     consultasRoute,
+    liquidacionesRoute,
     defensaRoute,
     denunciasRoute
 ];
