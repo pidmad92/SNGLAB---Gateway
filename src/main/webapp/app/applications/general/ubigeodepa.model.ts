ae842a3930513cad8f0c08a9e80bb256adada831
Christian Huangal Quiroz <chuangal@TRABAJO.GOB.PE>
2017-11-28 13:16:51 -0500

    Cambios para denuncias labora - incluye ubigeo

---
+++ src/main/webapp/app/applications/general/ubigeodepa.model.ts
@@ -0,0 +1,9 @@
+export class UbigeodepaModel {
+
+        constructor(
+            public coddep: string,
+            public descrip: string,
+            public coddepren: string
+        ) {
+        }
+    }
